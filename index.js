9fbcfea6fc09f65dbdad495ef8030bfe3856f6c9
alexis1986 <alexis.castellano@gmail.com>
2020-05-08 09:43:08 -0300

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+function A(props) {
+  return <p>Hola {props.nombre}!!!</p>;
+}
+
+function B(props) {
+  return <p>Puntaje de {props.nombre}: 10</p>;
+}
+
+class MiComponente extends Component {
+  render() {
+    return <p>Hola Mundooooo!!!!</p>;
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -12,12 +26,17 @@ class App extends Component {
   }
 
   render() {
+    const nombre = 'Alexis';
+
     return (
       <div>
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
         </p>
+        <MiComponente />
+        <A nombre={nombre}/>
+        <B nombre={nombre}/>
       </div>
     );
   }
